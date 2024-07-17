e969ef6d8c754ca337c9b6980e13f13abc66fbd9
bkphill2 <bkphill2@uw.edu>
2024-07-17 10:40:01 -0700

    Update dncnn.py

--- dncnn.py
+++ dncnn.py
@@ -2,7 +2,7 @@ import os #Import necessary libraries
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers, models, load_model
-from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from tensorflow.keras.preprocessing.image import img_to_array
 from sklearn.model_selection import train_test_split
 from PIL import Image
 from keras.layers import Input
