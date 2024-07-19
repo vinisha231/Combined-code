06744144ecedcfe2c71e44bc03e91c076f4505cc
Vinisha <129325340+vinisha231@users.noreply.github.com>
2024-07-19 16:12:09 -0700

    Update dncnn.py

--- dncnn.py
+++ dncnn.py
@@ -1,21 +1,20 @@
-import os #Import necessary libraries
+import os
+import struct
 import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras import layers, models, load_model
-from tensorflow.keras.preprocessing.image import img_to_array
-from sklearn.model_selection import train_test_split
-from PIL import Image
-from keras.layers import Input
-from keras.layers import Subtract
+from tensorflow.keras import layers, models, Input
+from tensorflow.keras.layers import Subtract
+from glob import glob
 
-def DnCNN(depth=17, filters=64, image_channels=1, use_bnorm=True):
+def DnCNN(depth=17, filters=64, image_channels=1, use_bn=True):
     input_layer = Input(shape=(None, None, image_channels), name='input')
-    x = layers.Conv2D(filters=filters, kernel_size=3, padding='same', activation='relu')(input_layer)
+    x = layers.Conv2D(filters, kernel_size=3, padding='same', activation='relu')(input_layer)
 
-    for i in range(depth - 2):
+    for _ in range(depth-2):
         x = layers.Conv2D(filters=filters, kernel_size=3, padding='same')(x)
-        if use_bnorm:
-            x = layers.BatchNormalization(axis=3)(x)
+        if use_bn:
+            x = layers.BatchNormalization()(x)
         x = layers.Activation('relu')(x)
 
     output_layer = layers.Conv2D(filters=image_channels, kernel_size=3, padding='same')(x)
@@ -23,4 +22,3 @@ def DnCNN(depth=17, filters=64, image_channels=1, use_bnorm=True):
     model = models.Model(inputs=input_layer, outputs=output_layer, name='DnCNN')
     return model
 
-model = DnCNN(depth=17, filters=64, image_channels=1, use_bnorm=True)
